bf456d19535c9df6fa073acf73cfb8c67b9e99c7
TJ <cooploops@gmail.com>
2017-11-10 16:34:25 -0800

    it works for minimum requirements

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-const inquirer = require("inquirer");
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -11,6 +11,89 @@ const connection = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
+function displayInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for(i=0;i<res.length;i++){
+          console.log(`
+          ID: ${res[i].item_id}
+          Product: ${res[i].product_name}
+          Department:${res[i].department_name}
+          Price:$${res[i].price}
+          Quantity:${res[i].stock_quantity}
+          -------------------------------------`);
+      };
+      start();      
+    });
+  }
+
+function displayPurchase(answer){
+    connection.query("SELECT price FROM products WHERE?",{item_id: answer.idList},function(err,res){
+        if (err) throw err;
+        let total = (answer.quantity * res[0].price);
+        console.log("Your total comes to " + total);
+        connection.end();
+    })
+}
+
+function updateInventory(answer, quantityLeft){
+    // console.log("update " + answer.idList);
+    // console.log("update " + quantityLeft);
+    connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity: quantityLeft},{item_id: answer.idList}],function(err,res){
+        if (err) throw err;
+        // console.log(res.affectedRows + " product added to cart!");
+        displayPurchase(answer)
+    });
+}
+
+function checkInventory(answer) {
+    // console.log("check " + answer.idList + " " + answer.quantity);
+    connection.query("SELECT stock_quantity FROM products WHERE ?",{item_id: answer.idList}, function(err, res){
+        if (err) throw err;
+        // console.log(res[0].stock_quantity);
+        if (res[0].stock_quantity < answer.quantity){
+            console.log("Insufficient quantity!");
+            start();
+        } else {
+            // console.log(res[0].stock_quantity);
+            var quantLeft = res[0].stock_quantity - answer.quantity;
+            // console.log(quantLeft);
+            updateInventory(answer, quantLeft);
+        }
+    })
+}
+
+function start(){
+    console.log("hit");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "idList",
+            message: "Which product ID would you like to buy?",
+            validate: function(value){
+                if(0<value && value<=10){
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many of that product would you like to purchase?",
+            validate: function(num){
+                if(isNaN(num) === false){
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(answer){
+        // console.log(answer);
+        checkInventory(answer);
+    })
+};
+
 connection.connect(function(err){
     if(err){
         console.error('error connecting: ' + err.stack);
@@ -19,35 +102,3 @@ connection.connect(function(err){
     console.log('connected as id ' + connection.threadId);
     displayInventory();
 });
-
-function displayInventory() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-    });
-  }
-
-inquirer.prompt([
-    {
-        type:"list",
-        name:"idList",
-        message:"Which product ID would you like to buy?",
-        choices:[1,2,3,4,5,6,7,8,9,10]
-    },
-    {
-        type:"input",
-        name:"quantity",
-        message: "How many of that product would you like to purchase?",
-        validate: function(value){
-            let regExp = /\d+/g
-            if(regExp.test(value) === true){
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-]).then(function(answer){
-
-    connection.end();
-})
